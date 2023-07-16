0dc2acc349f3c6cf0dc027b8a3f8b9d3f73ad760
Kevin <kramzyvine@icloud.com>
2023-07-16 16:39:32 +0300

    added Regular Expression Matching.py

---
+++ hard/Regular Expression Matching.py
@@ -0,0 +1,30 @@
+"""
+Given an input string s and a pattern p, implement regular expression matching with support for '.' and '*' where:
+->  '.' Matches any single character.​​​​
+->  '*' Matches zero or more of the preceding element.
+The matching should cover the entire input string (not partial).
+
+Input: s = "aa", p = "a"
+Output: false
+Explanation: "a" does not match the entire string "aa".
+
+Input: s = "aa", p = "a*"
+Output: true
+Explanation: '*' means zero or more of the preceding element, 'a'. Therefore, by repeating 'a' once, it becomes "aa".
+
+Input: s = "ab", p = ".*"
+Output: true
+Explanation: ".*" means "zero or more (*) of any character (.)".
+"""
+
+import re
+
+
+class Solution(object):
+    def isMatch(self, s, p):
+        p = '^' + p + '$'
+        pattern = re.compile(p)
+        if pattern.match(s):
+            return True
+        else:
+            return False
