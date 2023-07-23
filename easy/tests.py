968cf28a81dbe8c54c969dfb9204309e89582893
Kevin <kramzyvine@icloud.com>
2023-07-23 22:39:40 +0300

    added Replace Elements with Greatest Element on Right Side.py

--- easy/tests.py
+++ easy/tests.py
@@ -1,10 +1,3 @@
 a = [1, 2, 3, 4, 5]
-for i in range(0, len(a), 2):
-    try:
-        x = a[i + 1]
-        a[i + 1] = a[i]
-        a[i] = x
-    except:
-        pass
-
-print(a)
+y = a.pop(-1)
+print(a, y)
