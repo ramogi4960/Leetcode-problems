77a35547fb7432fba2faf4606f26c127ef02a80c
Kevin <kramzyvine@icloud.com>
2023-10-03 14:09:05 +0300

    added How Many Numbers Are Smaller Than the Current Number.py

--- tests.py
+++ tests.py
@@ -1,3 +1,3 @@
-x = "Kevin"
-x = x.replace(x[3], '')
-print(x)
+from platform import platform, machine, processor, system, version, python_implementation, python_version_tuple
+
+print(python_version_tuple())
