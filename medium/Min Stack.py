b3318fcb4f58f13e0d84e5ed36f3f2a1e95aa469
Kevin <kramzyvine@icloud.com>
2023-10-11 09:23:37 +0300

    made some minor changes to Min Stack.py

--- medium/Min Stack.py
+++ medium/Min Stack.py
@@ -52,9 +52,4 @@ class MinStack:
         return min(self.stack)
 
 
-# Your MinStack object will be instantiated and called as such:
-# obj = MinStack()
-# obj.push(val)
-# obj.pop()
-# param_3 = obj.top()
-# param_4 = obj.getMin()
+
--- tests.py
+++
@@ -1,2 +0,0 @@
-for i in range(10000, 10110):
-    print(chr(i), end=" ")
