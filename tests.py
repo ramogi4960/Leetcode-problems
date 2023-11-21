840c6af44f6856b14a49583398e3cfbdcb25572e
Kevin <kramzyvine@icloud.com>
2023-11-21 09:33:11 +0300

    added Minimum Amount of Time to Collect Garbage.py

--- tests.py
+++ tests.py
@@ -1,4 +1,10 @@
-s = "aabababa"
+def rev(number: int) -> int:
+    final_number = 0
+    while number:
+        final_number = (final_number * 10) + (number % 10)
+        number //= 10
 
-s = s.replace(f"a", "")
-print(s)
+    return final_number
+
+
+print(rev(123450000))
