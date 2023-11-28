744c03f7487a65104bb5f072c3dfc3b2b722104a
Kevin <kramzyvine@icloud.com>
2023-11-29 00:11:36 +0300

    added Find Median from Data Stream.py

--- tests.py
+++ tests.py
@@ -1,10 +1,1 @@
-def rev(number: int) -> int:
-    final_number = 0
-    while number:
-        final_number = (final_number * 10) + (number % 10)
-        number //= 10
-
-    return final_number
-
-
-print(rev(123450000))
+print(-1 > -2)
