24ed6aae798d91eadd3aceef25211004be852136
Kevin <kramzyvine@icloud.com>
2023-12-12 10:08:12 +0300

    added Element Appearing More Than 25% In Sorted Array.py

---
+++ easy/Element Appearing More Than 25% In Sorted Array.py
@@ -0,0 +1,28 @@
+"""
+Given an integer array sorted in non-decreasing order, there is exactly one integer in the array that occurs more than
+25% of the time, return that integer.
+
+
+
+Example 1:
+
+Input: arr = [1,2,2,6,6,6,6,7,10]
+Output: 6
+Example 2:
+
+Input: arr = [1,1]
+Output: 1
+
+
+Constraints:
+
+1 <= arr.length <= 104
+0 <= arr[i] <= 105
+"""
+
+
+class Solution:
+    def findSpecialInteger(self, arr: [int]) -> int:
+        for item in arr:
+            if arr.count(item) > len(arr) / 4:
+                return item
