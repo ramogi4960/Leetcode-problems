82c2951ffc4ed77e937f1c3a237bcfa9528d555a
Kevin <kramzyvine@icloud.com>
2023-12-23 10:43:36 +0300

    added Path Crossing.py

---
+++ easy/Path Crossing.py
@@ -0,0 +1,48 @@
+"""
+Given a string path, where path[i] = 'N', 'S', 'E' or 'W', each representing moving one unit north, south, east, or west
+, respectively. You start at the origin (0, 0) on a 2D plane and walk on the path specified by path.
+
+Return true if the path crosses itself at any point, that is, if at any time you are on a location you have previously
+visited. Return false otherwise.
+
+
+
+Example 1:
+
+
+Input: path = "NES"
+Output: false
+Explanation: Notice that the path doesn't cross any point more than once.
+Example 2:
+
+
+Input: path = "NESWW"
+Output: true
+Explanation: Notice that the path visits the origin twice.
+
+
+Constraints:
+
+1 <= path.length <= 104
+path[i] is either 'N', 'S', 'E', or 'W'.
+"""
+
+
+class Solution:
+    def isPathCrossing(self, path: str) -> bool:
+        track = [[0, 0], ]
+        for item in path:
+            x = list(track[-1])
+            if item == "N":
+                x[1] += 1
+            elif item == "E":
+                x[0] += 1
+            elif item == "S":
+                x[1] -= 1
+            else:
+                x[0] -= 1
+
+            if x in track: return True
+            else: track.append(x)
+
+        return False
