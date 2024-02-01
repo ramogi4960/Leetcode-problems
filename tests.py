d172431e744d9c5e37136c3f8c6a27206b66e458
Kevin <kramzyvine@icloud.com>
2024-02-01 08:32:37 +0300

    added Divide Array Into Arrays With Max Difference.py

--- tests.py
+++ tests.py
@@ -1,3 +1,2 @@
-from platform import machine, processor, system, version, python_implementation, python_version, python_version_tuple
-
-print(*python_version_tuple(), sep="\n")
+for i in range(10, 0, -1):
+    print(i)
