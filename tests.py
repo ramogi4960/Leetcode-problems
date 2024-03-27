4bcd1428fa5a7a945d141549af85456bc9ec54d7
Kevin <kramzyvine@icloud.com>
2024-03-27 18:57:11 +0300

    added Validate Stack Sequences.py

--- tests.py
+++ tests.py
@@ -1,1 +1,20 @@
-print(list("Kevin"))
+import asyncio
+
+
+async def main():
+    task = asyncio.create_task(inner())
+    print("One", end=" ")
+    await asyncio.sleep(3)
+    print("Four", end=" ")
+    await asyncio.sleep(1)
+    print("Five")
+
+
+async def inner():
+    print("Two", end=" ")
+    await asyncio.sleep(1)
+    print("Three", end=" ")
+
+
+asyncio.run(main())
+
