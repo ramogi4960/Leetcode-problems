b739c1e6c43e30ce0d59bf8657942c3d8c3a1ed4
Kevin <kramzyvine@icloud.com>
2024-05-03 15:54:37 +0300

    added Valid Sudoku.py

---
+++ medium/Valid Sudoku.py
@@ -0,0 +1,65 @@
+"""
+Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+Each row must contain the digits 1-9 without repetition.
+Each column must contain the digits 1-9 without repetition.
+Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+Note:
+
+A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+Only the filled cells need to be validated according to the mentioned rules.
+
+
+Example 1:
+
+
+Input: board =
+[["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: true
+Example 2:
+
+Input: board =
+[["8","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: false
+Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+
+
+Constraints:
+
+board.length == 9
+board[i].length == 9
+board[i][j] is a digit 1-9 or '.'.
+"""
+
+
+class Solution:
+    def isValidSudoku(self, board: [[str]]) -> bool:
+        for i in range(len(board)):
+            x = [item for item in board[i] if item != "."]
+            if len(set(board[i])) != len(x) + 1: return False
+            x = [board[item][i] for item in range(len(board)) if board[item][i] != "."]
+            if len(set(x)) != len(x): return False
+        for i in range(0, len(board), 3):
+            for j in range(0, len(board[0]), 3):
+                x = board[i][j:j + 3]
+                x += board[i + 1][j:j + 3]
+                x += board[i + 2][j:j + 3]
+                x = [item for item in x if item != "."]
+                if len(set(x)) != len(x): return False
+        return True
